15a8b67175e21b9c7491d27be61f9fc7ff022bce
JunBeomKwon <jbkwon@edentns.com>
2023-04-30 01:21:57 +0900

    최초 커밋

---
+++ WeatherScope/WeatherScope/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WeatherScope.Models;
+using WeatherScope.Services;
+
+namespace WeatherScope.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly WeatherService _weatherService;
+
+        public HomeController(WeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // 예시 위치 (위도, 경도)
+            double latitude = 37.7749;
+            double longitude = -122.4194;
+
+            Weather weather = await _weatherService.GetWeatherAsync(latitude, longitude);
+            return View(weather);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WeatherScope/WeatherScope/Models/Weather.cs
@@ -0,0 +1,19 @@
+﻿namespace WeatherScope.Models
+{
+    public class Weather
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string CityName { get; set; }
+        public string Description { get; set; }
+        public double Temperature { get; set; }
+        public double FeelsLike { get; set; }
+        public double TempMin { get; set; }
+        public double TempMax { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
+        public double WindSpeed { get; set; }
+        public int WindDirection { get; set; }
+    }
+
+}

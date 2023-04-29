7fee64a11052aea78467fc73549d626752929b72
JunBeomKwon <jbkwon@edentns.com>
2023-04-30 01:39:44 +0900

    Weather 클래스 수정

--- WeatherScope/WeatherScope/Models/Weather.cs
+++ WeatherScope/WeatherScope/Models/Weather.cs
@@ -1,19 +1,67 @@
-﻿namespace WeatherScope.Models
+﻿using System.Text.Json.Serialization;
+
+namespace WeatherScope.Models
 {
     public class Weather
     {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        [JsonPropertyName("coord")]
+        public Coord Coordinates { get; set; }
+
+        [JsonPropertyName("weather")]
+        public WeatherDetails[] WeatherDetails { get; set; }
+
+        [JsonPropertyName("main")]
+        public Main Main { get; set; }
+
+        [JsonPropertyName("wind")]
+        public Wind Wind { get; set; }
+
+        [JsonPropertyName("name")]
         public string CityName { get; set; }
-        public string Description { get; set; }
-        public double Temperature { get; set; }
-        public double FeelsLike { get; set; }
-        public double TempMin { get; set; }
-        public double TempMax { get; set; }
-        public int Pressure { get; set; }
-        public int Humidity { get; set; }
-        public double WindSpeed { get; set; }
-        public int WindDirection { get; set; }
     }
 
+    public class Coord
+    {
+        [JsonPropertyName("lon")]
+        public double Longitude { get; set; }
+
+        [JsonPropertyName("lat")]
+        public double Latitude { get; set; }
+    }
+
+    public class WeatherDetails
+    {
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+    }
+
+    public class Main
+    {
+        [JsonPropertyName("temp")]
+        public double Temperature { get; set; }
+
+        [JsonPropertyName("feels_like")]
+        public double FeelsLike { get; set; }
+
+        [JsonPropertyName("temp_min")]
+        public double TempMin { get; set; }
+
+        [JsonPropertyName("temp_max")]
+        public double TempMax { get; set; }
+
+        [JsonPropertyName("pressure")]
+        public int Pressure { get; set; }
+
+        [JsonPropertyName("humidity")]
+        public int Humidity { get; set; }
+    }
+
+    public class Wind
+    {
+        [JsonPropertyName("speed")]
+        public double Speed { get; set; }
+
+        [JsonPropertyName("deg")]
+        public int Direction { get; set; }
+    }
 }

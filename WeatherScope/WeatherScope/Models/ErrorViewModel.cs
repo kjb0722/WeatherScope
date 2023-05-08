241bc407bf1c5f0d0b4c3780d22761cd37ae2268
JunBeomKwon <jbkwon@edentns.com>
2023-05-08 19:27:33 +0900

    커밋

--- WeatherScope/WeatherScope/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WeatherScope.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
